e3a0c7922531aaa8eb9a1528f0b6a369a21b2da5
ceoread <ceoread@westfall.800ceoread.com>
2010-01-13 10:47:30 -0600

    Changes to security and about page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  def check_admin_access
+    ip = request.env["REMOTE_ADDR"]
+    is_admin = false
+    if ip =~ /^192\.168\./ or ip =~ /^10\./ or ip =~ /^172\.16\./ # or ip =~ /^your\.ip\.address\.here/
+      is_admin = true
+    end
+    return is_admin
+  end
 end
--- app/helpers/public_helper.rb
+++ app/helpers/public_helper.rb
@@ -1,7 +1,7 @@
 module PublicHelper
   def admin_link_to(link)
     ip = request.env["REMOTE_ADDR"]
-    if ip =~ /^192\.168\./ or ip =~ /^10\./ or ip =~ /^172\.16\./ 
+    if check_admin_access()
       link
     else
       ""
