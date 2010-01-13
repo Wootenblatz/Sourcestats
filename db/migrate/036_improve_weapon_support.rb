38834813c053b31ec4e6f1c228512f1e481c4818
ceoread <ceoread@westfall.800ceoread.com>
2010-01-13 14:52:39 -0600

    Fixed error in migration

--- db/migrate/036_improve_weapon_support.rb
+++ db/migrate/036_improve_weapon_support.rb
@@ -1,7 +1,7 @@
 class ImproveWeaponSupport < ActiveRecord::Migration
   def self.up
     add_column :weapons, :highlight, :string, :limit => 3, :default => "No"
-    add_column :weapons, :player_title, :string. :limit => 40
+    add_column :weapons, :player_title, :string, :limit => 40
   end
   
   def self.down
